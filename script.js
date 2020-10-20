f02c13d107fb5a4aa980d50b4273f7f8378d0fa3
haninaaaaaaaaa <jnnbenandiel@gmail.com>
2020-10-20 10:32:52 +0800

    update

--- script.js
+++ script.js
@@ -46,20 +46,11 @@ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 
-/******************** START PHOTO GRID *************/
-// SCRIPT FOR GRID
-
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("row");
-
-// Declare a loop variable
-var i;
-
 
 
 
 // SCERIPT FOR FILTER
-filterSelection("all")
+filterSelection("row")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("row");
@@ -103,4 +94,64 @@ for (var i = 0; i < btns.length; i++) {
   });
 } 
 
+/****  IMAGGE GGRID  */
 
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("row");
+
+// Declare a loop variable
+var i;
+
+function one() {
+  for (i = 0; i < elements.length; i++) {
+  elements[i].style.msFlex = "100%";  // IE10
+  elements[i].style.flex = "100%";
+}
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {  
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+/********** SLIDESHOW GALLERY *******/
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
